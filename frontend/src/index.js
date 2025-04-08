595e63c4ffc864b36b3b6ee67f4c5a380210344e
Ruturaj <ruturajnikam1010@gmail.com>
2025-04-08 23:26:30 +0530

    Initial setup for frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <body>
+      <h1>Har har Mahadev</h1>
+    </body>
+  </React.StrictMode>
+);
